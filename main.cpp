6b528c65f646eff6a17494952ae146bc8150b47f
lobanovam <71985042+lobanovam@users.noreply.github.com>
2022-08-16 18:34:32 +0300

    universal print func, check func, 5th try

--- main.cpp
+++ main.cpp
@@ -5,17 +5,15 @@
 void getCoef(float * u, float * v, float * d);
 void linear(float b, float c, float * u);
 void quadratic(float a, float b, float c, float * u, float * v, int * d);
-void printAnswLinear(float answ1);
-void printAnswQuad(float answ1, float answ2);
+void printAnsw(float answ1, float answ2, int option);
+void check(float a, float b, float c);
 
 int main()
 {
     float a = 0.0;
     float b = 0.0;
     float c = 0.0;
-    float answ1 = 0.0;
-    float answ2 = 0.0;
-    int check = 1;
+
 
     printf("This program solves the equations of this type: ");
     printf("a*x^2 + b*x + c = 0\n");
@@ -23,28 +21,7 @@ int main()
 
     getCoef(&a, &b, &c);
 
-    if (a == 0 && b == 0) {
-        if (c != 0) {
-            printf("%f = 0 has no solutions\n", c);
-            return 0;
-        } else {
-            printf("there are an infinite number of solutions\n");
-            return 0;
-        }
-    }
-
-    if (a == 0) {
-        printf("it's a linear equation\n");
-        linear(b, c, &answ1);
-        printAnswLinear(answ1);
-
-    } else {
-        printf("it's a quadratic equation\n");
-        quadratic (a, b, c, &answ1, &answ2, &check);
-        if(check)
-        printAnswQuad(answ1, answ2);
-
-    }
+    check(a, b, c);
 
     printf("Hope that you are satisfied\n");
     return 0;
@@ -54,20 +31,14 @@ int main()
 
 void getCoef(float * u, float * v, float * d)    //u = &a, v = &b, d = &c
 {
-    float a = 0.0;
-    float b = 0.0;
-    float c = 0.0;
     char ch = '0';
 
-    while (scanf("%f %f %f", &a, &b, &c) != 3) {
+    while (scanf("%f %f %f", u, v, d) != 3) {
         printf("incorrect enter, try again\n");
         while ((ch = getchar()) != '\n') {
         continue;
         }
     }
-    *u = a;
-    *v = b;
-    *d = c;
 }
 
 
@@ -85,7 +56,7 @@ void quadratic(float a, float b, float c, float * u, float * v, int * d)  //u = 
 {
     float disc = 0;
     if ((disc = b*b - 4*a*c) < 0) {
-            printf("there are no real solutions\n");
+            printf("since discriminant < 0, ");
             *d = 0;
 
         } else if (disc == 0) {
@@ -99,21 +70,50 @@ void quadratic(float a, float b, float c, float * u, float * v, int * d)  //u = 
         }
 }
 
-void printAnswLinear(float answ1)
+void printAnsw(float answ1, float answ2, int option)
 {
+    if (option == 0)
+    printf("there are no real solutions\n");
+    if (option == 1)
+    printf("there are an infinite number of solutions\n");
+    if (option == 2)
     printf("this solution is %f\n", answ1);
-
-}
-
-void printAnswQuad(float answ1, float answ2)
-{
-    if (answ1 == answ2) {
-        printf("this solution is %f\n", answ1);
-    } else {
-        printf("first solution is %f, second one is %f.\n", answ1, answ2);
+    if (option == 3) {
+        if (answ1 == answ2) {
+            printf("this solution is %f\n", answ1);
+        } else {
+        printf("first solution is %f, second is %f\n", answ1, answ2);
+        }
     }
 
 
+}
+
+void check(float a, float b, float c)
+{
+    float answ1 = 0.0;
+    float answ2 = 0.0;
+    int check = 1;
+    int option = 0;
+
+    if (a == 0 && b == 0) {
+        if (c == 0)
+        option = 1;
+    }
+    else if (a == 0) {
+        printf("it's a linear equation\n");
+        linear(b, c, &answ1);
+        option = 2;
+
+    } else {
+        printf("it's a quadratic equation\n");
+        quadratic (a, b, c, &answ1, &answ2, &check);
+        if(check)
+        option = 3;
+
+    }
+
+    printAnsw(answ1, answ2, option);
 }
 
 
