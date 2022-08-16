78223dd34e6f4499ec76606dc8310bb26ed43cf8
lobanovam <71985042+lobanovam@users.noreply.github.com>
2022-08-16 15:24:55 +0300

    separated in func (3rd try)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
+float disc(float a, float b, float c);
+void linear(float b, float c);
+void quadratic(float a, float b, float c);
+
 int main()
 {
     float a = 0.0;
@@ -12,7 +16,6 @@ int main()
     printf("This program solves the equations of this type: ");
     printf("a*x^2 + b*x + c = 0\n");
     printf("Enter quadratic equation coefficients a, b, c: \n");
-
 
     while (scanf("%f %f %f", &a, &b, &c) != 3) {
         printf("incorrect enter, try again\n");
@@ -32,25 +35,54 @@ int main()
     }
     if (a == 0) {
         printf("it's a linear equation\n");
-        printf("answer is %f\n", -c/b);
+        linear(b, c);
 
 
     } else {
         printf("it's a quadratic equation\n");
-        if ((disc = b*b - 4*a*c) < 0) {
-            printf("there are no real solutions\n");
-            return 0;
+        quadratic (a, b, c);
+    }
+    printf("Hope that you are satisfied\n");
+    return 0;
+}
 
-        } else if (disc == 0) {
+
+
+float disc(float a, float b, float c)
+{
+    return (b*b - 4*a*c);
+}
+
+
+
+void linear(float b, float c)
+{
+    printf("answer is %f\n", -c/b);
+}
+
+
+
+void quadratic(float a, float b, float c)
+{
+    if (disc(a, b, c) < 0) {
+            printf("there are no real solutions\n");
+
+        } else if (disc(a, b, c) == 0) {
             printf("oh, full square!\n");
             printf("answer is %f\n", -b/(2*a));
 
         } else {
             printf("first answer is %f, second is %f\n",
-            (-b - sqrt(disc))/(2*a), (-b + sqrt(disc))/(2*a) );
+            (-b - sqrt(disc(a, b, c))/(2*a), (-b + sqrt(disc(a, b, c))/(2*a))));
         }
-
-    }
-    printf("Hope that you are satisfied\n");
-    return 0;
 }
+
+
+
+
+
+
+
+
+
+
