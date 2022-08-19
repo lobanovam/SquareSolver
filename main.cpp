bfdc9b870aa595ae74d0fee90bbfdfdec8815c03
lobanovam <71985042+lobanovam@users.noreply.github.com>
2022-08-19 15:22:26 +0300

    better, test() func and tester() func

--- main.cpp
+++ main.cpp
@@ -8,51 +8,43 @@ void linear(float b, float c, float * u);
 void quadratic(float a, float b, float c, float * u, float * v, int * d);
 void printAnsw(float answ1, float answ2, int option);
 void solver(float a, float b, float c, float*ans1, float*ans2, int*u);
+void tester(float answ1, float answ2, float res1, float res2, int i);
 bool isEqual(float num1, float num2);
+void tests();
 
 
 int main(int args, char * argv[])
 {
 
     float a = 0.0, b = 0.0, c = 0.0, answ1 = 0.0, answ2 = 0.0;
-    int option = 0;
+    int option = 0, resct = 0, choice = 0, ch = '0';
 
-    FILE *pIn = NULL;
+
 
     printf("This program solves the equations of this type: ");
     printf("a*x^2 + b*x + c = 0\n");
-    printf("Enter quadratic equation coefficients a, b, c: \n");
 
-
-    int testCount = 0;
-
-    if ((pIn = fopen("tests.txt", "r")) == NULL) {
-        printf("can't open file\n");
-        return 0;
-
-    } else {
-        printf("file is opened\n");
+    printf("press 1 to input coefficients yourself, press 2 to run tests\n");
+    while((scanf("%d", &choice) !=1) || (choice != 1 && choice != 2)) {
+        printf("please press 1 or 2\n");
+        while((ch = getchar()) != '\n')
+        continue;
     }
 
-    fscanf(pIn, "%d", &testCount);
-    printf("%d\n", testCount);
-
-    for (int i = 0; i < testCount; i++) {
-        fscanf(pIn, "%f %f %f", &a, &b, &c);
+    if(choice == 1) {
+        getCoef(&a, &b, &c);
         solver(a, b, c, &answ1, &answ2, &option);
         printAnsw(answ1, answ2, option);
-        printf("*********************\n");
 
+    }else {
+        tests();
     }
-    fclose(pIn);
-    //getCoef(&a, &b, &c);
-
-
-
-
     printf("Hope that you are satisfied\n");
     return 0;
 }
+
+
+
 
 
 
--- solver.cpp
+++ solver.cpp
@@ -8,6 +8,8 @@ void quadratic(float a, float b, float c, float * u, float * v, int * d);
 void printAnsw(float answ1, float answ2, int option);
 void solver(float a, float b, float c, float*ans1, float*ans2, int*u);
 bool isEqual(float num1, float num2);
+void tester(float answ1, float answ2, float res1, float res2, int i);
+void tests();
 
 void quadratic(float a, float b, float c, float * u, float * v, int * d)  //u = &answ1, v = &answ2 d = &check
 {
@@ -15,6 +17,7 @@ void quadratic(float a, float b, float c, float * u, float * v, int * d)  //u = 
     if ((disc = b*b - 4*a*c) < 0) {
             printf("since discriminant < 0, ");
             *d = 0;
+            *u = *v = 0;
 
         } else if (disc == 0) {
             printf("there is only one solution!\n");
@@ -75,7 +78,7 @@ void solver(float a, float b, float c, float*ans1, float*ans2, int *u ) //u = &o
 void getCoef(float * u, float * v, float * d)    //u = &a, v = &b, d = &c
 {
     char ch = '0';
-
+    printf("Enter quadratic equation coefficients a, b, c: \n");
     while (scanf("%f %f %f", u, v, d) != 3) {
         printf("incorrect enter, try again\n");
         while ((ch = getchar()) != '\n') {
@@ -96,3 +99,56 @@ void linear(float b, float c, float * u)    //u = &ans1
     *u = -c/b;
 
 }
+
+void tester(float answ1, float answ2, float res1, float res2, int i)
+{
+    if ((isEqual(res1, answ1) && isEqual(res2, answ2))||(isEqual(res2, answ1) && isEqual(res1, answ2))) {
+            printf("test #%d is passed\n", i+1);
+        }else {
+            printf("test #%d is failed\n", i+1);
+            printf("expected solutions are %f, %f\n", res1, res2);
+        }
+}
+
+
+void tests() {
+    float a = 0.0, b = 0.0, c = 0.0, answ1 = 0.0, answ2 = 0.0, res1 = 0.0, res2 = 0.0;
+    int option = 0, resct = 0, testCount = 0;
+
+    FILE *pIn = NULL;
+    if ((pIn = fopen("tests.txt", "r")) == NULL) {
+        printf("can't open file\n");
+        return;
+
+    } else {
+        printf("file is opened\n");
+    }
+
+    fscanf(pIn, "%d", &testCount);
+    printf("there is %d tests in total\n", testCount);
+
+    for (int i = 0; i < testCount; i++) {
+        fscanf(pIn,"%f %f %f %d", &a, &b, &c, &resct);
+
+        if (resct == 0) {
+            res1 = res2 = 0;
+
+        } else if (resct == 1) {
+            fscanf(pIn, "%f", &res1);
+            res2 = res1;
+        }else {
+            fscanf(pIn, "%f %f", &res1, &res2);
+
+        }
+
+        solver(a, b, c, &answ1, &answ2, &option);
+        printAnsw(answ1, answ2, option);
+        tester(answ1, answ2, res1, res2, i);
+
+        printf("***********************\n");
+
+    }
+    fclose(pIn);
+    return;
+}
+
