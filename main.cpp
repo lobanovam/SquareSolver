ae3e24ad9e22b942049c0d58ce7512c96b434f76
lobanovam <71985042+lobanovam@users.noreply.github.com>
2022-08-23 17:28:31 +0300

    only docs left

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 #define INF_ROOTS 3
 
 void getCoef(float *a, float *b, float *c);
-void linear(float b, float c, float *answ1_pt);
-void quadratic(float a, float b, float c, float *answ1_pt, float *answ2_pt, int *check_pt);
+void linear(float b, float c, float *answ1_pt, int *rootcnt_pt);
+void quadratic(float a, float b, float c, float *answ1_pt, float *answ2_pt, int *rootcnt_pt);
 void printAnsw(float answ1, float answ2, int rootct);
 void solver(float a, float b, float c, float *ans1, float *ans2, int *rootcnt_pt);
 bool isEqual(float num1, float num2);
@@ -21,7 +21,7 @@ int main()
 {
 
     float a = NAN, b = NAN, c = NAN, answ1 = NAN, answ2 = NAN;
-    int rootcnt = 0, resct = 0, choice = 0, ch = '0';//ct - cnt
+    int rootcnt = 0, resct = 0, choice = 0;
 
     printf("This program solves the equations of this type: ");
     printf("a*x^2 + b*x + c = 0\n");
@@ -43,27 +43,30 @@ int main()
     return 0;
 }
 
-void quadratic(float a, float b, float c, float *answ1_pt , float *answ2_pt, int *check_pt)  //u = &answ1, v = &answ2 d = &check
+void quadratic(float a, float b, float c, float *answ1_pt , float *answ2_pt, int *rootcnt_pt)
 {
     assert(answ1_pt != NULL);
     assert(answ2_pt != NULL);
-    assert(check_pt != NULL);
+    assert(rootcnt_pt != NULL);
+    assert(answ1_pt != answ2_pt);
 
     float disc = 0;
     if ((disc = b*b - 4*a*c) < 0) {
-            printf("since discriminant < 0, ");//убрать?//asserts
-            *check_pt = 0;
+            fprintf(stderr, "disc < 0\n");//убрать?//asserts
+            *rootcnt_pt = 0;
             *answ1_pt = *answ2_pt = 0;
 
         } else if (disc == 0) {
-            printf("there is only one solution!\n");
+            fprintf(stderr, "disc = 0\n");
             *answ1_pt = *answ2_pt = (-b/(2*a));
+            *rootcnt_pt = 1;
 
         } else {
             float sqrt_disc = sqrt(disc);
-            printf("there are two solutions!\n");
+            fprintf(stderr, "disc > 0\n");
             *answ1_pt = ((-b + sqrt_disc)/(2*a));
             *answ2_pt = ((-b - sqrt_disc)/(2*a));
+            *rootcnt_pt = 2;
         }
 }
 
@@ -75,10 +78,10 @@ void printAnsw(float answ1, float answ2, int rootcnt)
     if (rootcnt == INF_ROOTS)
     printf("there are an infinite number of solutions\n");
     if (rootcnt == 1)
-    printf("this great solution is %f\n", answ1);
+    printf("there is only one solution, it is %f\n", answ1);
     if (rootcnt == 2) {
         if (isEqual(answ1, answ2)) {
-            printf("this solution is %f\n", answ1);
+            printf("one solution: %f\n", answ1);
         } else {
         printf("first solution is %f, second is %f\n", answ1, answ2);
         }
@@ -87,36 +90,22 @@ void printAnsw(float answ1, float answ2, int rootcnt)
 
 }
 
-void solver(float a, float b, float c, float *answ1_pt, float *answ2_pt, int *rootcnt_pt ) //u = &rootct, ans1 = &answ1, ans2 = &answ2
+void solver(float a, float b, float c, float *answ1_pt, float *answ2_pt, int *rootcnt_pt )
 {
-
-    *rootcnt_pt = 0;
     int check = 1;
-
-    if (isEqual(a, 0) && isEqual(b, 0) ) {
-        if (isEqual(c, 0))
-        *rootcnt_pt = INF_ROOTS;
-    }
-    else if (isEqual(a, 0)) {
-        printf("it's a linear equation\n");
-        linear(b, c, answ1_pt);
-
-        *rootcnt_pt = 1;
-
+    if (isEqual(a, 0)) {
+        linear(b, c, answ1_pt, rootcnt_pt);
+        *answ2_pt = *answ1_pt;
     } else {
-        printf("it's a quadratic equation\n");
-        quadratic (a, b, c, answ1_pt, answ2_pt, &check);
-        if(check)
-        *rootcnt_pt = 2;
+        quadratic(a, b, c, answ1_pt, answ2_pt, rootcnt_pt);
     }
-
-
 }
 
 void getCoef(float *a, float *b, float *c)
 {
-    char ch = '0';
+    char ch = 0;
     printf("Enter quadratic equation coefficients a, b, c: \n");
+
     while (scanf("%f %f %f", a, b, c) != 3) {
         printf("incorrect enter, try again\n");
         clearBuff();
@@ -128,11 +117,24 @@ bool isEqual(float num1, float num2)
  return(fabs(num1 - num2) < EPSILON);
 }
 
-void linear(float b, float c, float *answ1_pt)    //u = &ans1
+void linear(float b, float c, float *answ1_pt, int *rootcnt_pt)
 {
+    if (isEqual(b, 0)) {
+        fprintf(stderr, "nonlinear\n");
 
- printf("linear equation has one solution\n");//изменнить
-    *answ1_pt = -c/b;
+        if (isEqual(c, 0)) {
+            *rootcnt_pt = INF_ROOTS;
+
+        } else {
+            *rootcnt_pt = 0;
+        }
+    } else {
+        fprintf(stderr, "linear\n");
+        *rootcnt_pt = 1;
+        *answ1_pt = -c / b;
+
+    }
+
 
 }
 
@@ -140,32 +142,33 @@ void tester(float answ1, float answ2, float res1, float res2, int i)
 {
     if ((isEqual(res1, answ1) && isEqual(res2, answ2))||(isEqual(res2, answ1) && isEqual(res1, answ2))) {
             printf("test #%d is passed\n", i+1);
+
         }else {
             printf("test #%d is failed\n", i+1);
+
             if (isEqual(res1, res2)) {
                 printf("expected solution is %f.\n", res1);
+
             } else {
                 printf("expected solutions are %f, %f\n", res1, res2);
             }
         }
 }
 
-
 void tests() {
-    float a = 0.0, b = 0.0, c = 0.0, answ1 = 0.0, answ2 = 0.0, res1 = 0.0, res2 = 0.0;
+    float a = NAN, b = NAN, c = NAN, answ1 = NAN, answ2 = NAN, res1 = NAN, res2 = NAN;
     int rootcnt = 0, rescnt = 0, testCount = 0;
 
     FILE *pIn = NULL;
-    if ((pIn = fopen("tests.txt", "r")) == NULL) {
-        printf("can't open file\n");
-        return;
+    pIn = fopen("tests.txt", "r");
 
-    } else {
-        printf("file is opened\n");
-    }
+    assert(pIn != NULL);
 
     fscanf(pIn, "%d", &testCount);
-    printf("there are %d tests in total\n\n", testCount);
+
+    assert(testCount != 0);
+
+    fprintf(stderr, "there are %d tests in total\n\n", testCount);
 
     for (int i = 0; i < testCount; i++) {
         fscanf(pIn,"%f %f %f %d", &a, &b, &c, &rescnt);
@@ -192,9 +195,9 @@ void tests() {
     return;
 }
 
-void(clearBuff())
+void clearBuff()
 {
-    char ch = ' ';
+    char ch = 0;
     while((ch = getchar()) != '\n')
     continue;
 
